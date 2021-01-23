3847c6c56137ad6dadf297c4b54c353b3a1c42ed
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-01-23 17:04:08 +0800

    1/23 17.

--- 45_2D_GAME/Assets/Script/Player.cs
+++ 45_2D_GAME/Assets/Script/Player.cs
@@ -47,7 +47,7 @@ public class Player : MonoBehaviour
     private void Move()
     {
         float h = Input.GetAxis("Horizontal");
-        rig.velocity = new Vector2(h * speed, rig.velocity.y);
+        rig.velocity = new Vector2(h * speed, rig.velocity.x);
        
     }
 
