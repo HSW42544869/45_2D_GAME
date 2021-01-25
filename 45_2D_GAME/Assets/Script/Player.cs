7d1fc5461a1ee4eedd81c1399cf687edbee49337
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-01-25 16:38:11 +0800

    1/25 16.

--- 45_2D_GAME/Assets/Script/Player.cs
+++ 45_2D_GAME/Assets/Script/Player.cs
@@ -2,14 +2,14 @@
 
 public class Player : MonoBehaviour
 {
-    [Header("移動速度"), Range(0.1f,1)]
+    [Header("移動速度"), Range(0.1f,10)]
     public float speed = 0.1f;
     [Header("是否在地板上"), Tooltip("用來儲存玩家是否站在地板上")]
     public bool isGround = false;
 
 
     [Header("法術"), Tooltip("存放要生成的法術預置物")]
-    public GameObject Spells;
+    public GameObject spells;
     [Header("法術生成點"), Tooltip("法術生成的起始位置")]
     public Transform point;
     [Header("法術速度"), Range(0, 500)]
@@ -26,7 +26,19 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        //移動
         Move();
+        //射擊動作
+        if (Input.GetMouseButton(0))
+        {
+            ani.SetBool("攻擊開關", true);
+            Spell();
+        }
+        else
+        {
+            ani.SetBool("攻擊開關", false);
+        }
+        
     }
     private void Awake()
     {
@@ -46,17 +58,45 @@ public class Player : MonoBehaviour
     /// </summary>
     private void Move()
     {
+        //水平浮點數 = 輸入 的 取得軸向("水平") - 左右AD
         float h = Input.GetAxis("Horizontal");
-        rig.velocity = new Vector2(h * speed, rig.velocity.x);
-       
+        // 鋼體 的 速度 = 新 二為向量(水平浮點數 * 速度，剛體的加入度的y)
+        rig.velocity = new Vector2(h * speed, rig.velocity.y);
+        ani.SetBool("跑步開關",h != 0);
+        //垂直浮點數 = 輸入 的 取得軸向("垂直") - 上下WS
+        float v = Input.GetAxis("Vertical");
+        // 剛體 的 速度 = 新 二為向量(垂直浮點數 * 速度，剛體的加速度的x)
+        rig.velocity = new Vector2(v * speed, rig.velocity.x);
+        ani.SetBool("跑步開關", v != 0);
+        //走路方向向右
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        //走路方向向左
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        
+        ani.SetBool("上跑開關", h > 0);
+        ani.SetBool("下跑開關", h < 0);
+        
+
     }
 
     /// <summary>
     /// 施法功能
     /// </summary>
-    private void Fire()
+    private void Spell()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject spellsIns = Instantiate(spells, point.transform.position, point.transform.rotation);
+            spellsIns.GetComponent<Rigidbody2D>().AddForce(transform.right * Speedspelles);
+
+            
+        }
     }
 
     /// <summary>
