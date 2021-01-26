34dd02224d956f8a2675e6c618a66a5d7088b5f3
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-01-26 16:17:31 +0800

    1/26 16.

--- 45_2D_GAME/Assets/Script/Player.cs
+++ 45_2D_GAME/Assets/Script/Player.cs
@@ -46,6 +46,7 @@ public class Player : MonoBehaviour
         //抓到角色身上的剛體元件存放到 rig 欄位內
         rig = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
+        aud = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -92,10 +93,13 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            //音源 的 播放一次音效(音效，隨機大小聲)
+            aud.PlayOneShot(soundFire, Random.Range(0.8f, 1.5f));
+            //生成 子彈在槍口
+            //生成(物件，座標，角度)
             GameObject spellsIns = Instantiate(spells, point.transform.position, point.transform.rotation);
             spellsIns.GetComponent<Rigidbody2D>().AddForce(transform.right * Speedspelles);
-
-            
+  
         }
     }
 
