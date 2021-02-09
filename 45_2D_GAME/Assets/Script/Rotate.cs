3e0cea2900120ec47c91b2dcca9b83aff3e2c9b6
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-02-10 01:12:49 +0800

    2/10 1.

---
+++ 45_2D_GAME/Assets/Script/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float Rotatespeed = 50;
+    public float DestroyTime = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, DestroyTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(gameObject.transform.up);
+    }
+}
