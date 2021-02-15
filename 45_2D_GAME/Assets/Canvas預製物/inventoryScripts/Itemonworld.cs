5f53d2b027b8eb877449d4d75f1bcf987949d013
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-02-16 03:28:36 +0800

    2/16 3.

---
+++ 45_2D_GAME/Assets/Canvas預製物/inventoryScripts/Itemonworld.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Itemonworld : MonoBehaviour
+{
+    public Item thisItem;
+    public Intentory playerInventory;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            AddNewItem();
+            Destroy(gameObject);
+        }
+    }
+    public void AddNewItem()
+    {
+        if (!playerInventory.itemList.Contains(thisItem))
+        {
+            playerInventory.itemList.Add(thisItem);
+            //InventoryManager.CreatNewItem(thisItem);
+        }
+        else
+        {
+            thisItem.itemHeld += 1;
+        }
+        InventoryManager.RefreshItem();
+    }
+}
---
+++ 45_2D_GAME/Assets/Canvas預製物/inventoryScripts/Slot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public Item slotItem;
+    public Image slotImage;
+    public Text slotNum;
+
+    public void ItemOnClicked()
+    {
+        InventoryManager.UpdateItemInfo(slotItem.iteminfo);
+    }
+}
