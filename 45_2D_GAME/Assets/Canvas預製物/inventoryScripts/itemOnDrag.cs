edc841739ef09e3b68327737eac1c4eba26fc296
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-02-24 08:40:17 +0800

    2/24 8.

--- 45_2D_GAME/Assets/Canvas預製物/inventoryScripts/itemOnDrag.cs
+++ 45_2D_GAME/Assets/Canvas預製物/inventoryScripts/itemOnDrag.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class itemOnDrag : MonoBehaviour,IBeginDragHandler ,IDragHandler ,IEndDragHandler
+public class itemOnDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
     public Transform originalParent;
@@ -24,27 +24,40 @@ public class itemOnDrag : MonoBehaviour,IBeginDragHandler ,IDragHandler ,IEndDra
     }
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (eventData.pointerCurrentRaycast.gameObject.name == "item image")//判斷下面物體明子是: Item Image 那麼互換位置
+        if (eventData.pointerCurrentRaycast.gameObject != null)
         {
-            transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform.parent.parent);
-            transform.position = eventData.pointerCurrentRaycast.gameObject.transform.parent.parent.position;
-            //itemList的物品存儲位址改變
-            var temp = mybag.itemList[currentItemID];
-            mybag.itemList[currentItemID] = mybag.itemList[eventData.pointerCurrentRaycast.gameObject.GetComponentInParent<Slot>().slotID];//鼠標點及物體，且找到正確的目標ID
-            mybag.itemList[eventData.pointerCurrentRaycast.gameObject.GetComponentInParent<Slot>().slotID] = temp;
+            if (eventData.pointerCurrentRaycast.gameObject.name == "item image")//判斷下面物體明子是: Item Image 那麼互換位置
+            {
+                transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform.parent.parent);
+                transform.position = eventData.pointerCurrentRaycast.gameObject.transform.parent.parent.position;
+                //itemList的物品存儲位址改變
+                var temp = mybag.itemList[currentItemID];
+                mybag.itemList[currentItemID] = mybag.itemList[eventData.pointerCurrentRaycast.gameObject.GetComponentInParent<Slot>().slotID];//鼠標點及物體，且找到正確的目標ID
+                mybag.itemList[eventData.pointerCurrentRaycast.gameObject.GetComponentInParent<Slot>().slotID] = temp;
 
-            eventData.pointerCurrentRaycast.gameObject.transform.parent.position = originalParent.position;
-            eventData.pointerCurrentRaycast.gameObject.transform.parent.SetParent(originalParent);
-            GetComponent<CanvasGroup>().blocksRaycasts = true;//射線阻擋開啟，不然無法再次選中移動物品
-            return;
+                eventData.pointerCurrentRaycast.gameObject.transform.parent.position = originalParent.position;
+                eventData.pointerCurrentRaycast.gameObject.transform.parent.SetParent(originalParent);
+                GetComponent<CanvasGroup>().blocksRaycasts = true;//射線阻擋開啟，不然無法再次選中移動物品
+                return;
+            }
+            //否則直接掛在檢測到slot下面
+            if (eventData.pointerCurrentRaycast.gameObject.name == "slot(Clone)")
+            {
+                transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform);
+                transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+                //itemList的物品儲存位置改變
+                mybag.itemList[eventData.pointerCurrentRaycast.gameObject.GetComponentInParent<Slot>().slotID] = mybag.itemList[currentItemID];
+                //解決自己放在自己位置的問題
+                if (eventData.pointerCurrentRaycast.gameObject.GetComponent<Slot>().slotID != currentItemID)
+                    mybag.itemList[currentItemID] = null;
+
+                GetComponent<CanvasGroup>().blocksRaycasts = true;
+                return;
+            }
         }
-        //否則直接掛在檢測到slot下面
-        transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform);
-        transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
-        //itemList的物品儲存位置改變
-        mybag.itemList[eventData.pointerCurrentRaycast.gameObject.GetComponentInParent<Slot>().slotID]= mybag.itemList[currentItemID];
-        mybag.itemList[currentItemID] = null;
-
-        GetComponent<CanvasGroup>().blocksRaycasts = true;
+            //其他任何位置都歸位
+            transform.SetParent(originalParent);
+            transform.position = originalParent.position;
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
     }
-}
